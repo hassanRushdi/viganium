48dfd56e9415f7634b752b570cbf8cd7f9562245
hassanRushdi <khater312@gmail.com>
2025-05-01 10:58:32 +0300

    config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,19 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    images: {
+        unoptimized: true,
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'viganium.com',
+                port: '',
+                pathname: '/**'
+            }
+        ]
+      },
+};
 
 export default withNextIntl(nextConfig);
